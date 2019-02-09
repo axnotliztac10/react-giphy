b9200ea7d53b29b3eab1ee8b80c1e9b69cb8afb9
Metabaron <metabaron@Metabarons-MacBook-Pro.local>
2019-02-09 00:28:13 -0600

    Add routing and main containers

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+import Home from 'containers/Home';
+
+export function App() {
+  return (
+    <Switch>
+      <Redirect exact from="/" to="/home" />
+      <Route path="/home" component={Home}/>
+      <Route path="/favorites" component={Home}/>
+    </Switch>
+  );
 }
 
-export default App;
---
+++ src/containers/Favorites/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default class Favorites extends React.PureComponent {
+  render() {
+    return <div />
+  }
+}
