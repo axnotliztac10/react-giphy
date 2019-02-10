ae4c250f3f3ede45f0fe94a5fd38ceb80e87d949
Metabaron <metabaron@Metabarons-MacBook-Pro.local>
2019-02-10 09:04:55 -0600

    Add loader placeholder for every gif

--- src/components/GiphyCard/index.js
+++ src/components/GiphyCard/index.js
@@ -4,27 +4,55 @@ import { Image, Button, Text } from 'rebass';
 import Card from 'components/Card';
 import addButton from 'images/add.png';
 import addedButton from 'images/added.png';
+import loader from 'images/loader.svg';
 
-export default function GiphyCard({ giphy, onAddRemoveFavorite }) {
-  return (
-    <Card
-      p={2}
-      my={3}
-      mx={2}
-      width={[1, 1/3, 1/4]}
-    >
-      <Image width={1} src={giphy.images.downsized_medium.url} />
-      <Button
-        p={0}
-        width={1}
-        variant="outline"
-        bg="transparent"
-        onClick={() => onAddRemoveFavorite(giphy)}
+export default class GiphyCard extends React.PureComponent {
+  state = {
+    loaded: false
+  }
+
+  onLoad = () => {
+    this.setState({
+      loaded: true
+    });
+  }
+
+  render() {
+    const { loaded } = this.state;
+    const { giphy, onAddRemoveFavorite } = this.props;
+    const Img = () => (
+      <img
+        alt={giphy.title}
+        style={{
+          maxWidth: '100%',
+          display: (loaded) ? 'block' : 'none'
+        }}
+        src={giphy.images.downsized_medium.url}
+        onLoad={this.onLoad}
+      />
+    );
+
+    return (
+      <Card
+        p={2}
+        my={3}
+        mx={2}
+        width={[1, 1/3, 1/4]}
       >
-        <Text pb={2} color="black">{giphy.title}</Text>
-        {!giphy.isFavorite && <Image src={addButton} width={1/4} />}
-        {giphy.isFavorite && <Image src={addedButton} width={1/4} />}
-      </Button>
-    </Card>
-  );
+        <Img />
+        {!loaded && <img src={loader} alt="loading" />}
+        <Button
+          p={0}
+          width={1}
+          variant="outline"
+          bg="transparent"
+          onClick={() => onAddRemoveFavorite(giphy)}
+        >
+          <Text pb={2} color="black">{giphy.title}</Text>
+          {!giphy.isFavorite && <Image src={addButton} width={1/4} />}
+          {giphy.isFavorite && <Image src={addedButton} width={1/4} />}
+        </Button>
+      </Card>
+    );
+  }
 }
